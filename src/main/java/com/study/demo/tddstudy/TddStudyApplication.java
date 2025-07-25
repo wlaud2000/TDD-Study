4b62932d15c066a2f4a135a9ec09914974151dc0
Jimyeong Kim <kjm021221@icloud.com>
2025-07-25 10:35:59 +0900

    begin a study

---
+++ src/main/java/com/study/demo/tddstudy/TddStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.demo.tddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddStudyApplication.class, args);
+    }
+
+}
